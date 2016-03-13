1255391929366ed68f83563c0656d128a7020ff3
Altakh <altakh@yahoo.com>
2016-03-13 17:21:07 +0500

    Task 3

---
+++ db.sh
@@ -0,0 +1,4 @@
+mysql -u root -p
+create database ask_db;
+grant all privileges on ask_db.* to admin@localhost identified by '123';
+
--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+gunicorn ask.wsgi:application --bind 0.0.0.0:8000
 
 
